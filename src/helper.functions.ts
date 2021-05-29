9a9e6e9a414ebbd2e4f8640fca85490c445d9802
D3h4n <shawn2012tt@live.com>
2021-05-29 19:07:02 -0400

    Refactor parsing commands

--- src/helper.functions.ts
+++ src/helper.functions.ts
@@ -1,6 +1,6 @@
-import { Message } from 'discord.js';
 import commands from './commands/index.commands.setup';
 import config from './utils/config';
+import { Message } from 'discord.js';
 import { client } from './index';
 
 // functions
@@ -17,38 +17,35 @@ const log = function logEveryCommand({
   );
 };
 
-const parseCommand = function (cmd: string) {
+const parseCommand = function (input: string) {
   let args: string[] = []; //args array
   let word = ''; // store each arg
 
   // check if quotations are completed
-  if ((cmd.match(/"/g)?.length ?? 0) % 2 !== 0) {
+  if ((input.match(/"/g)?.length ?? 0) % 2 !== 0) {
     throw new Error('Incomplete quotes');
   }
 
   // add space to the end of the command
-  cmd += ' ';
+  input += ' ';
 
-  for (let i = 0; i < cmd.length; i++) {
+  for (let i = 0; i < input.length; i++) {
     // if space add word to args and clear word
-    if (cmd[i] === ' ') {
-      if (word) {
-        args.push(word);
-      }
-
-      word = '';
+    if (input[i] !== ' ' && input[i] !== '"') {
+      // add character to word
+      word += input[i];
       continue;
     }
 
-    if (cmd[i] === '"') {
-      // add to word characters until next quote
-      for (i++; cmd[i] !== '"' && i < cmd.length; i++) {
-        word += cmd[i];
+    // if quote add all characters between quotes
+    if (input[i] === '"') {
+      for (i++; input[i] !== '"' && i < input.length; i++) {
+        word += input[i];
       }
-    } else {
-      // add character to word
-      word += cmd[i];
     }
+
+    word && args.push(word);
+    word = '';
   }
 
   return args;
@@ -74,11 +71,8 @@ export default function handleMessage(message: Message) {
     let commandName = args[0].toLowerCase();
 
     // check for the correct command and execute it
-    let command = commands.get(commandName);
-    if (command) {
-      command.action(message, args);
-      log(message);
-      return;
-    }
+    commands.get(commandName)?.action(message, args);
+
+    log(message);
   }
 }
