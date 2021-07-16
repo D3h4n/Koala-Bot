5bcf205331b924678494344dbc43b4020fe0d4b6
D3h4n <shawn2012tt@live.com>
2021-07-16 18:09:22 -0400

    yeet was missing for some reason

---
+++ src/commands/admin/yeet.config.ts
@@ -0,0 +1,60 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Command from '../common.commands.config';
+
+export default class yeetCommand extends Command {
+  constructor() {
+    super(
+      'Yeet',
+      'yeet',
+      [
+        'Move a bunch of people between voice channels',
+        'NB:// You have to be in the voice channel',
+        'Usage: $yeet <new voice channel>',
+      ],
+      [],
+      ['MOVE_MEMBERS']
+    );
+  }
+
+  action({ member, guild, channel }: Message, args: string[]) {
+    const voiceChannel = member?.voice.channel; // connected voice channel
+
+    // get channel name
+    const channelName = args.slice(1).join(' ');
+
+    // check if user is in a voice channel
+    if (!voiceChannel) {
+      channel.send('`Gotta be in a channel buddy`');
+      return;
+    }
+
+    // find the new channel
+    const newChannel = guild?.channels.cache.find(
+      ({ type, name }) => type === 'voice' && name === channelName
+    );
+
+    // check if channel was found
+    if (!newChannel) {
+      channel.send('`Could not find that channel`');
+      return;
+    }
+
+    // move each connected member to new channel
+    voiceChannel?.members.forEach((member) =>
+      member.voice.setChannel(newChannel)
+    );
+
+    // create and send response
+    const response = new MessageEmbed();
+
+    response
+      .setTitle(`Yote from "${voiceChannel.name}" to "${newChannel.name}"`)
+      .setDescription(
+        voiceChannel.members
+          .array()
+          .map((member, idx) => `${idx + 1}. ${member.displayName}`)
+      );
+
+    channel.send(response);
+  }
+}
